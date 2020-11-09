54e669954457bf3d9a2da78dd7ab76edf638078c
theoperdriel <tperdriel@gmail.com>
2020-11-09 14:13:01 +0100

    loi de proba + insertion ordre chronologique

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # TP Simulation
 import random
+import numpy
 
 class Evenement:
 
-    def __init__(self,pEvenement,pDate):
+    def __init__(self,pNomEvenement,pDate):
         #Evenement
-        self.evenement = pEvenement
+        self.nomEvenement = pNomEvenement
         #Date
         self.dateEvenement = pDate
 
@@ -18,6 +19,17 @@ class CentreDeMaintenance :
     Qc,Qr,Bc,Br=0,0,0,0
     echeancier = []
 
+    def insertEvenement(self, evenement):
+        notInserted = True
+        i = 0
+        while (notInserted and i < len(self.echeancier)):
+            if (self.echeancier[i].dateEvenement >= evenement.dateEvenement):
+                self.echeancier.insert(i, evenement)
+                notInserted = False
+            i += 1
+        if (i == len(self.echeancier)):
+            self.echeancier.append(evenement)
+
     def __init__(self,pDate):
 
         #Date simualtion en heures
@@ -25,10 +37,10 @@ class CentreDeMaintenance :
 
     def debutSimulation(self):
         print("DebutSimulation")
-        evenement = Evenement("arriveeBus",self.dateSimulation+0.5)
-        self.echeancier.append(evenement)
+        evenement = Evenement("arriveeBus",self.dateSimulation + numpy.random.exponential(2))
+        self.insertEvenement(evenement)
         evenement = Evenement("finSimulation", 160)
-        self.echeancier.append(evenement)
+        self.insertEvenement(evenement)
 
     def finSimulation(self):
         print("Fin Simualtion")
@@ -40,36 +52,35 @@ class CentreDeMaintenance :
 
     def arriveeBus(self):
         print("arriveeBus")
-        evenement = Evenement("arriveeBus", self.dateSimulation + 0.5)
-        self.echeancier.append(evenement)
+        evenement = Evenement("arriveeBus", self.dateSimulation + numpy.random.exponential(2))
+        self.insertEvenement(evenement)
         self.NbBus += 1
         evenement = Evenement("arriveeFileC", self.dateSimulation)
-        self.echeancier.append(evenement)
+        self.insertEvenement(evenement)
 
     def arriveeFileC(self):
         print("Arrivé file controle")
         self.Qc += 1
         if(self.Bc == 0):
             evenement = Evenement("accesControle", self.dateSimulation)
-            self.echeancier.append(evenement)
+            self.insertEvenement(evenement)
 
     def accesControle(self):
         print("Acces controle")
         self.Qc -= 1
         self.Bc = 1
-        evenement = Evenement("departControle", self.dateSimulation+0.25)
-        self.echeancier.append(evenement)
+        evenement = Evenement("departControle", self.dateSimulation + random.uniform(0.25, 1.0833))
+        self.insertEvenement(evenement)
 
     def departControle(self):
         print("Depart controle")
         self.Bc = 0
         if (self.Qc > 0):
             evenement = Evenement("accesControle", self.dateSimulation)
-            self.echeancier.append(evenement)
-        x = random()
-        if(x<0.3):
+            self.insertEvenement(evenement)
+        if(random.random() < 0.33):
             evenement = Evenement("arriveeFileR", self.dateSimulation)
-            self.echeancier.append(evenement)
+            self.insertEvenement(evenement)
 
     def arriveeFileR(self):
         print("Arrivé file reparation")
@@ -77,21 +88,21 @@ class CentreDeMaintenance :
         self.NbBusRep += 1
         if (self.Br < 2):
             evenement = Evenement("accesReparation", self.dateSimulation)
-            self.echeancier.append(evenement)
+            self.insertEvenement(evenement)
 
     def accesReparation(self):
         print("Arrivé reparation")
         self.Qr -= 1
         self.Br += 1
-        evenement = Evenement("departReparation", self.dateSimulation + 2.1)
-        self.echeancier.append(evenement)
+        evenement = Evenement("departReparation", self.dateSimulation + random.uniform(2.1, 4.5))
+        self.insertEvenement(evenement)
 
     def departReparation(self):
         print("Depart reparation")
         self.Br -= 1
         if(self.Qr>0):
             evenement = Evenement("debutSimulation", self.dateSimulation)
-            self.echeancier.append(evenement)
+            self.insertEvenement(evenement)
 
 
     def mise_A_Jour_Aires(self,D1,D2):
@@ -101,7 +112,7 @@ class CentreDeMaintenance :
 
 if __name__ == '__main__':
 
-    DateSimulation = 00
+    DateSimulation = 0
     centreMaintenance = CentreDeMaintenance(DateSimulation)
     evenement = Evenement("debutSimulation", centreMaintenance.dateSimulation)
     centreMaintenance.echeancier.append(evenement)
@@ -111,23 +122,23 @@ if __name__ == '__main__':
         centreMaintenance.mise_A_Jour_Aires(centreMaintenance.dateSimulation,evt.dateEvenement)
         centreMaintenance.dateSimulation = evt.dateEvenement
 
-        if(evt.evenement == "debutSimulation"):
+        if(evt.nomEvenement == "debutSimulation"):
             centreMaintenance.debutSimulation()
-        elif (evt.evenement == "finSimulation"):
+        elif (evt.nomEvenement == "finSimulation"):
             centreMaintenance.finSimulation()
-        elif (evt.evenement == "arriveeBus"):
+        elif (evt.nomEvenement == "arriveeBus"):
             centreMaintenance.arriveeBus()
-        elif (evt.evenement == "arriveeFileC"):
+        elif (evt.nomEvenement == "arriveeFileC"):
             centreMaintenance.arriveeFileC()
-        elif (evt.evenement == "accesControle"):
+        elif (evt.nomEvenement == "accesControle"):
             centreMaintenance.accesControle()
-        elif (evt.evenement == "departControle"):
+        elif (evt.nomEvenement == "departControle"):
             centreMaintenance.departControle()
-        elif (evt.evenement == "arriveeFileR"):
+        elif (evt.nomEvenement == "arriveeFileR"):
             centreMaintenance.arriveeFileR()
-        elif (evt.evenement == "accesReparation"):
+        elif (evt.nomEvenement == "accesReparation"):
             centreMaintenance.accesReparation()
-        elif (evt.evenement == "departReparation"):
+        elif (evt.nomEvenement == "departReparation"):
             centreMaintenance.departReparation()
         else:
             print("evenement inconnu")
